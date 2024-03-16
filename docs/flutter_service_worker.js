0dcb650e325952031e99cbba1a9b9e6452aeca3b
hensybex <kamenetskiyto@gmail.com>
2024-03-16 19:39:31 +0300

    Reverted

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"main.dart.js": "c34f930105937fb6c9900f3fb9ea9c52",
+const RESOURCES = {"main.dart.js": "3442c2afff26acadda1952f37217b954",
 "assets/assets/icons/car_doctor.png": "161545500693b513401aecc1e3530e52",
 "assets/assets/icons/home.png": "f314b297037a3beeb7cdb8c009a5fafc",
 "assets/assets/icons/pin_staff.png": "1f15d7340da4dbddf009c227e64ddbee",
@@ -61,8 +61,8 @@ const RESOURCES = {"main.dart.js": "c34f930105937fb6c9900f3fb9ea9c52",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
 "canvaskit/skwasm.js.symbols": "741d50ffba71f89345996b0aa8426af8",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
-"index.html": "9bed5447d860beb6a69c00e695f90ad6",
-"/": "9bed5447d860beb6a69c00e695f90ad6",
+"index.html": "a290e08be87d9ca72bcd7d2b350d3bf7",
+"/": "a290e08be87d9ca72bcd7d2b350d3bf7",
 "version.json": "397843f9054663615800eef8eab6c62c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
